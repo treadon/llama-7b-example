edc6b9beb38689f3d2d1c57405ed14ea287a69e1
Ritesh Khanna <ritesh@clipwiregames.com>
2023-03-14 14:26:04 -0400

    Llama 7B example

---
+++ launch.ps1
@@ -0,0 +1,17 @@
+$venvName = ".env"
+
+if (Test-Path $venvName)
+{
+    Write-Host "Virtual environment $venvName already exists."
+} else
+{
+    Write-Host "Creating virtual environment $venvName"
+    python -m venv $venvName
+}
+
+& ".\$venvName\Scripts\Activate.ps1"
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118
+pip install git+https://github.com/huggingface/transformers.git@refs/pull/21955/merge
+pip install sentencepiece
+
+python .\src\llama.py
